24d975cf62e4519d3b2bd060607ce51f01c23a11
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-07-22 16:18:30 +0100

    created programatically navigable button

--- pages/books.js
+++ pages/books.js
@@ -1,3 +1,12 @@
+import Link from "next/link";
+
 export default function BooksPage() {
-  return <div className="bg-green-100">Books Page</div>;
+  return (
+    <div className="bg-green-100">
+      Books Page{" "}
+      <div>
+        <Link href="/">Go to home page</Link>
+      </div>{" "}
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,23 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 export default function Home() {
-  return <div className="w-full py-10 bg-red-100">Hello</div>;
+  const router = useRouter();
+
+  const handleClick = () => {
+    alert("you clicked the button!");
+    router.push("/books");
+  };
+
+  return (
+    <div className="w-full py-10 bg-red-100">
+      Hello{" "}
+      <button className="bg-blue-500 px-2 py-1 rounded" onClick={handleClick}>
+        Click me
+      </button>
+      <div>
+        <Link href="/books">Go to books page</Link>
+      </div>
+    </div>
+  );
 }
