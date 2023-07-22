28ed94e5d8891ab3f081adc8428d87d57a8970f3
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-07-22 17:53:00 +0100

    created BookGrid component

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import BookGrid from "@/components/home/book-grid";
+
 export default function Home() {
   return (
     <div>
@@ -5,26 +7,8 @@ export default function Home() {
         Tomasz
       </div>
       <div className="text-center text-gray-700">I am a bootcamp student</div>
-      <div id="book-grid" className="mt-20 grid grid-cols-3 w-full bg-red-100">
-        <div id="book1" className="bg-green-100 p-4">
-          <p className="title mb-4 text-center text-xl font-medium">
-            Game of thrones
-          </p>
-          <p className="author text-sm text-gray-700 text-center">
-            George RR Martin
-          </p>
-        </div>
 
-        <div id="book2">
-          <p className="title">Atomic Habits</p>
-          <p className="author">James Clear</p>
-        </div>
-
-        <div id="book3">
-          <p className="title">Born a Crime</p>
-          <p className="author">Trevor Noah</p>
-        </div>
-      </div>
+      <BookGrid></BookGrid>
     </div>
   );
 }
