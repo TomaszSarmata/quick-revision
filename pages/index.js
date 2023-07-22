3a098bce2898d1f50d30bfaccafedf2feb54551a
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-07-22 16:40:24 +0100

    erased previous content to start from scratch

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,5 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-
 export default function Home() {
-  const router = useRouter();
-
-  const handleClick = () => {
-    alert("you clicked the button!");
-    router.push("/books");
-  };
-
   return (
-    <div className="w-full py-10 bg-red-100">
-      Hello{" "}
-      <button className="bg-blue-500 px-2 py-1 rounded" onClick={handleClick}>
-        Click me
-      </button>
-      <div>
-        <Link href="/books">Go to books page</Link>
-      </div>
-    </div>
+    <div className="bg-red-100 text-5xl text-center my-10">Welcome 👋</div>
   );
 }
