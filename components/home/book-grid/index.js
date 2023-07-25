c22bb00f0066eff98d9ece55d0049d6968c684d2
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-07-25 10:31:53 +0100

    used map function to loop through the list of books and display as BookItem

--- components/home/book-grid/index.js
+++ components/home/book-grid/index.js
@@ -1,12 +1,18 @@
 import BookItem from "./book-item";
 
+const books = [
+  { title: "Game of thrones", author: "George RR Martin" },
+  { title: "Atomic Habits", author: "James Clear" },
+  { title: "Born a Crime", author: "Trevor Noah" },
+];
+
 export default function BookGrid() {
   return (
     <div>
       <div id="book-grid" className="mt-20 grid grid-cols-3 w-full bg-red-100">
-        <BookItem title="Game of thrones" author="George RR Martin"></BookItem>
-        <BookItem title="Atomic Habits" author="James Clear"></BookItem>
-        <BookItem title="Born a Crime" author="Trevor Noah"></BookItem>
+        {books.map((book) => {
+          return <BookItem title={book.title} author={book.author}></BookItem>;
+        })}
       </div>
     </div>
   );
